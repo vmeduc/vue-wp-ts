d4f0973392b2426d55c615fceb9ee30498f4c2ee
Vlad Melekhin <vmelekhin@wsoft.ru>
2020-07-17 18:12:56 +0300

    Введение Vuex

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+
 import App from 'pages/App.vue'
+
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+let store = new Vuex.Store({
+    state: {
+
+    },
+    mutations: {
+        
+    }
+})
 
 new Vue({
     el: '#app',
+    store,
     render: a => a(App)
 });
